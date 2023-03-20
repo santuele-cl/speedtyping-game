f7b5faecc59c191c9d1eb6ef64e09b0d12951b17
santuele.cl <santuele.cl@gmail.com>
2023-03-20 20:06:06 +0800

    Added word calculator function

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ textareaData, setTextareaData ] = useState('')
+    
+  const handleOnChange = event => {
+      setTextareaData(event.target.value)
+  }
+
+  const calculateWordCount = () => {
+    const filteredTextareaData = textareaData.split(' ').filter(item => item !== '')
+    return filteredTextareaData.length
+  }
+
+  const handleStart = () => {
+    console.log(calculateWordCount())
+  } 
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>How fast do you type?</h1>
+      <textarea value={textareaData} onChange={handleOnChange}/>
+      <h4>Time remaining: ???</h4>
+      <button onClick={handleStart}>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   )
 }
