09970e569419607f4f971db50770e82904736c6a
santuele.cl <santuele.cl@gmail.com>
2023-03-21 15:59:58 +0800

    Added autofucos on textarea and disable it by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [textareaData, setTextareaData] = useState('')
   const [timer, setTimer] = useState(5)
   const [hasStarted, setHasStarted] = useState(false)
+  const [wordCount, setWordCount] = useState(0)
+  const textareaRef = useRef(null)
 
-  console.log(timer)
-  
   useEffect(()=>{
     if(hasStarted && timer > 0) {
       setTimeout(()=>{
@@ -15,6 +15,7 @@ function App() {
       },1000)
     } 
     else if (timer === 0) {
+      setWordCount(calculateWordCount(textareaData))
       setHasStarted(false)
     }
   }, [hasStarted, timer])
@@ -23,33 +24,41 @@ function App() {
       setTextareaData(event.target.value)
   }
 
-  const calculateWordCount = () => {
-    const filteredTextareaData = textareaData.split(' ').filter(item => !!item)
+  const calculateWordCount = (tex) => {
+    const filteredTextareaData = tex.split(' ').filter(item => !!item)
     return filteredTextareaData.length
   }
 
   const handleStart = () => {
     if(!hasStarted) {
+      setTextareaData('')
+      setWordCount(0)
       setTimer(5)
     }
     setHasStarted(true)
-    console.log(calculateWordCount())
+    textareaRef.current.disabled = false
+    textareaRef.current.focus()
   } 
-
-  const handleReset = () => {
-      setTimer(5)
-      setHasStarted(true)
-  }
 
   return (
     <div className="App">
       <h1>How fast do you type?</h1>
-      <textarea value={textareaData} onChange={handleOnChange}/>
+      <textarea 
+        ref={textareaRef}
+        value={textareaData} 
+        onChange={handleOnChange}
+        disabled={!hasStarted}
+      />
       <h4>Time remaining: {timer} </h4>
       { 
-        <button onClick={handleStart} disabled={hasStarted}>Start</button> 
+        <button 
+          onClick={handleStart} 
+          disabled={hasStarted}
+        >
+          Start
+        </button> 
       }
-      <h1>Word count: ???</h1>
+      <h1>Word count: {wordCount ? wordCount : '???'}</h1>
     </div>
   )
 }
